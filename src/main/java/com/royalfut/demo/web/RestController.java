5ad3f62c35aa7237c2740427fe0a34a370d98ba0
SShmalikov <procris@mail.ru>
2021-06-24 20:53:21 +0300

    remove logging

--- src/main/java/com/royalfut/demo/web/RestController.java
+++ src/main/java/com/royalfut/demo/web/RestController.java
@@ -16,11 +16,10 @@ public class RestController {
 
     public static final String CLIENT_ID = "754137399246-qabm6770ckoo0puec4pibpbsh79pvi44.apps.googleusercontent.com";
     public static final String CLIENT_SECRET = "bsNTqgIBZ--F5EOfpuXee0k0";
-    public static final String REDIRECT_URI = "https://google-auth-royalfut.herokuapp.com/";
+    public static final String REDIRECT_URI = "https://google-auth-royalfut.herokuapp.com/storeauthcode";
 
     @PostMapping(path = "/storeauthcode")
     public Result storeAuthCode(HttpServletRequest request, @RequestBody String authCode) throws IOException {
-        System.out.println("Got requested authCode " + authCode);
         if (request.getHeader("X-Requested-With") == null) {
             // Without the `X-Requested-With` header, this request could be forged. Aborts.
         }
